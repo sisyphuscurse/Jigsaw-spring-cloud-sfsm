027353396362f37922d26054592076aa396acdc2
Barry Zhong <zhongdj@gmail.com>
2017-10-20 14:47:26 +0800

    EntityFactory是个抽象概念

--- order-service/src/main/java/com/dmall/order/domain/OrderEntityFactory.java
+++ order-service/src/main/java/com/dmall/order/domain/OrderEntityFactory.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 //TODO [Barry][SSM] 这也是因为Spring FSM带来的Boiler Plate，当中没有任何领域逻辑，没有任何状态机本身的信息
+//TODO [Barry] EntityFactory本身是一个抽象概念，未必是一个独立的类。聚合根可能是其他聚合的EntityFactory
 public class OrderEntityFactory {
 
   @Autowired
